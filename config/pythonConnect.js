f937e29457dfcd61befc1c46e6fe12fbf0afa754
Laksh <laksh.dang@gmail.com>
2021-03-18 10:48:04 -0400

    Attempt to fix python connect data as string error

--- config/pythonConnect.js
+++ config/pythonConnect.js
@@ -5,11 +5,6 @@ module.exports = {
 	postRequest: function (path, data, callback){
 		axios.post(pythonDir+path, data)
 		.then(result=>{
-			// console.log(typeof(result))
-			// console.log(typeof(result.data))
-			// if (typeof result.data === 'string' || result.data instanceof String)
-			// 	result.data = JSON.parse(result.data)
-			console.log(result.data)
 			callback(null, result);
 		})
 		.catch(error=>{
